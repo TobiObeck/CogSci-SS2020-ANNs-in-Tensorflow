f50adb7f853e7b39f8612e10f6454ab0e3bb7abb
Tobi Obeck <13554426+TobiObeck@users.noreply.github.com>
2020-11-10 13:47:39 +0100

    adds perceptron

--- homework02/perceptron.py
+++ homework02/perceptron.py
@@ -25,6 +25,8 @@ class Perceptron:
         # np.randon.randint(3)
 
     # Calculates the drive / activation.
+    # activation = every input is multiplied with every weight + bias
+    # wrapped by activation function
     # will the neuron be activated and pass 1 or 0 forward?
     # You could use @ as a matrix multiplication command.
     # weighted_sum = self.weights @ data + self.bias
@@ -32,22 +34,37 @@ class Perceptron:
         weighted_sum = np.dot(self.weights, input_data) + self.bias
         return self.threshold(weighted_sum)
 
-    # 2. Returns a 1 or a 0, depending on whether the perceptron 
+    # 2. Returns a 1 or a 0, depending on whether the perceptron
     # surpassed the threshold. int(...) is casting a bool to int
     def threshold(self, weighted_sum):
         return int(weighted_sum >= 0)
+    
 
+    # this method was given in live session
     # 1. forward step
     # 2. calculate weight updates
     # 3. bias updates
     # times 1, because bias is unweighted
-    def training_step(self, input, label):
-        prediction = self.forward_step(input)
-        self.weights += self.alpha * \
-            (label - prediction) * input  # plus or minus equals??
+    def training_step(self, sample, label):
+        prediction = self.forward_step(sample)
+        self.weights += self.alpha * (label - prediction) * sample
         self.bias += self.alpha * (label - prediction) * 1
         # + error term needed here somewhere?
+        # delta_weight = self.alpha * (label - prediction) * sample
+        # delta_bias = self.alpha * (label - prediction) * 1
+        # return [delta_weight, delta_bias]
 
+    # def update(self, delta):
+
+
+#### Miscellaneous functions
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
 
 possible_inputs = np.array([[0, 0],
                             [0, 1],
@@ -55,47 +72,47 @@ possible_inputs = np.array([[0, 0],
                             [1, 1]])
 
 # targets
-t_and = np.array([0, 0, 0, 1])
-t_or = np.array([0, 1, 1, 1])
-t_and = np.array([1, 1, 1, 0])
-t_or = np.array([1, 0, 0, 0])
-t_xor = np.array([0, 1, 1, 0])
+t_and = np.array([0,0,0,1])
+t_or = np.array([0,1,1,1])
+t_nand = np.array([1,1,1,0])
+t_nor = np.array([1,0,0,0])
+t_xor = np.array([0,1,1,0])
 
 # training
 # target that we are training on
 target = t_and
 
 # new perception instance
-INPUT_LENGTH = np.size(possible_inputs, 1) # length of sub-array (2)
+INPUT_LENGTH = np.size(possible_inputs, 1)  # length of sub-array (2)
 perceptron = Perceptron(INPUT_LENGTH)
 
 # training loop
 steps = []
 accuracies = []
 
-for i in range(500):
+for i in range(1500):
     steps.append(i)
 
     # draw a random sample from data
-    index = np.random.randint(len(possible_inputs))
-    sample = possible_inputs[index]
-    label = target[index]
+    rand_index = np.random.randint(len(possible_inputs))
+    sample = possible_inputs[rand_index]
+    label = target[rand_index]
 
     # perform a trianing step
     perceptron.training_step(sample, label)
 
     # how does our  perceptroin perform?
-    # perfonmrabnce overall four possible inputs
+    # performance overall four possible inputs
 
     accuracy_sum = 0
-    for j in range(len(possible_inputs)):
-        output = perceptron.forward_step(possible_inputs[j])
-        accuracy_sum += int(output == target[j])
+    for k in range(len(possible_inputs)):
+        output = perceptron.forward_step(possible_inputs[k])
+        accuracy_sum += int(output == target[k])
     accuracy = accuracy_sum/4
     accuracies.append(accuracy)
 
 for n in range(len(possible_inputs)):
-    output = perceptron.forward_step(possible_inputs[n])
+    output = perceptron.forward_step(possible_inputs[n])    
     print("Input: %s \t True Label: %d \t Perceptron's Prediction: %d" %
           (np.array2string(possible_inputs[n]), target[n], output))
 
@@ -104,5 +121,5 @@ plt.figure()
 plt.plot(steps, accuracies)
 plt.xlabel("Training Steps")
 plt.ylabel("Accuracy")
-#plt.ylim([-0.1, 1.2])
+plt.ylim([-0.1, 1.2])
 plt.show()
